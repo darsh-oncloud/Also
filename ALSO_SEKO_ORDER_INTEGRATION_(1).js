9c45d664617633e6c9c50995d7d546c2f40432bf
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-04-22 15:52:16 -0400

    Update from NetSuite (Auto)

--- ALSO_SEKO_ORDER_INTEGRATION_(1).js
+++ ALSO_SEKO_ORDER_INTEGRATION_(1).js
@@ -21,13 +21,13 @@ define(['N/search', 'N/record', 'N/log'], function (search, record, log) {
     var STATUS_SENT = '2';               // Sent
     var STATUS_ERROR = '3';              // Error
     var STATUS_NOT_RELEASED = '4';       // Not Released
-    var STATUS_PARTIAL_READY = '5';      // Partially Ready
-    var STATUS_PARTIAL_SENT = '6';       // Partially Sent
-    var STATUS_FULFILLED = '7';          // Fulfilled
-    var STATUS_PARTIAL_FULFILLED = '8';  // Partially Fulfilled
-    var STATUS_HOLD = '9';               // Hold
+    var STATUS_PARTIAL_READY = '6';      // Partially Ready
+    var STATUS_PARTIAL_SENT = '7';       // Partially Sent
+    var STATUS_FULFILLED = '8';          // Fulfilled
+    var STATUS_PARTIAL_FULFILLED = '9';  // Partially Fulfilled
+    var STATUS_HOLD = '10';               // Hold
 
-    var SEKO_LOCATION_ID = '16';
+    var SEKO_LOCATION_ID = '7';
     var IGNORE_ITEM_ID = '907';
 
     function getInputData() {
