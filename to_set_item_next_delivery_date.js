62a772a0964fa8aba44873dc2082718dd2fe5291
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-06-23 17:58:40 -0400

    Update from NetSuite (Auto)

--- to_set_item_next_delivery_date.js
+++ to_set_item_next_delivery_date.js
@@ -123,41 +123,41 @@ define(['N/search', 'N/record', 'N/format', 'N/log'], (search, record, format, l
             }
 
             try {
-                // const parsedDate = format.parse({ value: date, type: format.Type.DATE });
-                // record.submitFields({
-                //     type: recordType,
-                //     id: itemId,
-                //     values: { custitem_next_delivery_date: parsedDate },
-                //     options: { enableSourcing: false, ignoreMandatoryFields: true }
-                // });
+                const parsedDate = format.parse({ value: date, type: format.Type.DATE });
+                record.submitFields({
+                    type: recordType,
+                    id: itemId,
+                    values: { custitem_next_delivery_date: parsedDate },
+                    options: { enableSourcing: false, ignoreMandatoryFields: true }
+                });
 
-              const parsedDate = format.parse({
-    value: date,
-    type: format.Type.DATE
-});
+//               const parsedDate = format.parse({
+//     value: date,
+//     type: format.Type.DATE
+// });
 
-// Create GMT/UTC datetime from the delivery date
-// This sets the time to 00:00:00 GMT for that delivery date
-const gmtDateTime = new Date(Date.UTC(
-    parsedDate.getFullYear(),
-    parsedDate.getMonth(),
-    parsedDate.getDate(),
-    0,
-    0,
-    0
-));
+// // Create GMT/UTC datetime from the delivery date
+// // This sets the time to 00:00:00 GMT for that delivery date
+// const gmtDateTime = new Date(Date.UTC(
+//     parsedDate.getFullYear(),
+//     parsedDate.getMonth(),
+//     parsedDate.getDate(),
+//     0,
+//     0,
+//     0
+// ));
 
-record.submitFields({
-    type: recordType,
-    id: itemId,
-    values: {
-        custitem_next_delivery_date: gmtDateTime
-    },
-    options: {
-        enableSourcing: false,
-        ignoreMandatoryFields: true
-    }
-});
+// record.submitFields({
+//     type: recordType,
+//     id: itemId,
+//     values: {
+//         custitem_next_delivery_date: gmtDateTime
+//     },
+//     options: {
+//         enableSourcing: false,
+//         ignoreMandatoryFields: true
+//     }
+// });
 
 log.debug('Updated item ' + itemId, {
     nextDeliveryDateFromTO: date,
