5d7a5733482103e48c16232c3b72c8ef799065db
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-08-19 17:30:24 -0400

    Update from NetSuite (Auto)

--- netsuite_script.js
+++ netsuite_script.js
@@ -2,7 +2,7 @@
   "id": "",
   "name": "Unknown",
   "recordType": "",
-  "exportedAt": "2026-08-19T21:30:09.118Z",
+  "exportedAt": "2026-08-19T21:30:22.777Z",
   "criteria": [],
   "columns": [],
   "sort": []
